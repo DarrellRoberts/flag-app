738244300faf9a7257a0451071866ba431dd8f8c
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-09-11 13:01:45 +0200

    Updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import Flag from './components/Flag';
-import Layout from './components/Layout';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Flag from "./components/Flag";
+import Layout from "./components/Layout";
 
 function App() {
   const [country, setCountry] = useState([]);
 
   const fetchCountry = async () => {
-    const res = await fetch('https://restcountries.com/v3.1/all');
+    const res = await fetch("https://restcountries.com/v3.1/all");
     const data = await res.json();
     setCountry(data);
   };
@@ -23,7 +23,7 @@ function App() {
     <div className="app">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
+          <Route index element={<Homepage country={country} />} />
           <Route path="/:country" element={<Flag />} />
         </Route>
       </Routes>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,3 +1,21 @@
-export default function Homepage() {
-  return <main>main</main>;
+import { Link } from "react-router-dom";
+
+export default function Homepage({ country }) {
+  console.log(country);
+
+  return (
+    <main>
+      <div className="container-homepage">
+        {country &&
+          country.map((cnt) => (
+            <div key={cnt.flag.png}>
+              <Link to="/:country">
+                <img src={cnt.flags.png} alt={cnt.flags} />
+              </Link>
+              <div>{cnt.name.common}</div>
+            </div>
+          ))}
+      </div>
+    </main>
+  );
 }
